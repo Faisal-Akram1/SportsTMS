21f9ff94c094ff24c4b0c4439e3004570524729a
Faisal.Akram1 <eng.faisalbhalli@gmail.com>
2026-04-26 13:39:12 +0200

    Completed with the auth-services with some modifications

--- auth-service/src/routes/userRoutes.js
+++ auth-service/src/routes/userRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const authController = require('../controllers/authController');
 
-router.get('/', (req, res) => {
-  res.json({
-    message: 'Auth Service API is working'
-  });
-});
+router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 module.exports = router;
